37b923fe1291517b0ee8ecae25f90cef52ccbfc6
Sunny Sharma <123087797+sunnysharma4002@users.noreply.github.com>
2026-06-22 21:05:26 +0530

    Change environment variable access to use defaults

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-HIKERAPI_TOKEN = os.environ["HIKERAPI_TOKEN"]
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8871360008:AAGRwSdQz6A56zizaLwH0ZghEEgbJjoBbOA")
+HIKERAPI_TOKEN = os.environ.get("HIKERAPI_TOKEN", "v0vzeazsudyedfua41pcrtjh87kjf6tw")
